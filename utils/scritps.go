bb102c71bddc155f2d8b205acafe81c6840a6ac8
Rafael Azevedo <mail@rafaelazevedo.me>
2016-12-27 20:37:42 -0500

    Changed node name parameter in the outage script command

--- utils/scritps.go
+++ utils/scritps.go
@@ -3,8 +3,7 @@ package utils
 import "os/exec"
 
 func AssignNodeScript(node string) ([]byte, error) {
-	nodeName := "node_name=" + node
-	args := []string{"ADD", nodeName, "Request Via Outage API"}
+	args := []string{"ADD", node, "Request Via Outage API"}
 	cmd := exec.Command(OutageScript, args...)
 	out, err := cmd.Output()
 	if err != nil {
@@ -14,8 +13,7 @@ func AssignNodeScript(node string) ([]byte, error) {
 }
 
 func DeassignNodeScript(node string) ([]byte, error) {
-	nodeName := "node_name=" + node
-	args := []string{"REMOVE", nodeName, "Request Via Outage API"}
+	args := []string{"REMOVE", node, "Request Via Outage API"}
 	cmd := exec.Command(OutageScript, args...)
 	out, err := cmd.Output()
 	if err != nil {
